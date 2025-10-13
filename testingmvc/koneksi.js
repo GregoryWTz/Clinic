801fe7e5cd1cbdf4d6b4ff39693b5d89c36b315f
GregoryWTz <gwtlibra@gmail.com>
2025-10-13 13:43:45 +0700

    Delete testingmvc directory

--- testingmvc/koneksi.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("db_rumahsakit", "root", "", {
-  host: "192.168.56.1", // Windows host IP
-  port: 3307,
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
